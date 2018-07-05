6310b8b137c53e2cf8b43968117773ef6220899c
jcqnly <jcqnly@gmail.com>
2018-07-05 16:27:39 -0700

    test noodle and brand models with name getter tests

---
+++ NoodleApi/SetupTests/TestModels.cs
@@ -0,0 +1,36 @@
+﻿using NoodleApi.Models;
+using NoodleApi.Controllers;
+using System;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace SetupTests
+{
+    public class TestModels
+    {
+        [Fact]
+        public void NoodleNameGetterTest()
+        {
+            Noodle noodle = new Noodle();
+            noodle.Name = "Cheese Ramen";
+            noodle.Flavor = "Cheese";
+
+            noodle.Name = "Extra spicy ramen";
+
+            Assert.Equal("Extra spicy ramen", noodle.Name);
+        }
+
+        [Fact]
+        public void BrandNameGetterTest()
+        {
+            Brand brand = new Brand();
+            brand.Name = "Maruchan";
+            brand.Country = "Japan";
+
+            brand.Name = "Nestle";
+
+            Assert.Equal("Nestle", brand.Name);
+        }
+    }
+}
