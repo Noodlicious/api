27cfc4b41a006fdb42f92fc327e7a68a96189a91
stephendeanharper <stephendeanharper@gmail.com>
2018-07-06 10:27:24 -0700

    Added tests for all controllers

---
+++ NoodleApi/NoodleApiTest/TestModels.cs
@@ -0,0 +1,35 @@
+using NoodleApi.Models;
+using NoodleApi.Controllers;
+using System;
+using Xunit;
+using System.Linq;
+
+namespace NoodleApiTest
+{
+    public class TestModels
+    {
+        [Fact]
+        public void NoodleNameGetterTest()
+        {
+            Noodle noodle = new Noodle();
+            noodle.Name = "Cheese Ramen";
+            noodle.Flavor = "Cheese";
+
+            noodle.Name = "Extra spicy ramen";
+
+            Assert.Equal("Extra spicy ramen", noodle.Name);
+        }
+
+        [Fact]
+        public void BrandNameGetterTest()
+        {
+            Brand brand = new Brand();
+            brand.Name = "Maruchan";
+            brand.Country = "Japan";
+
+            brand.Name = "Nestle";
+
+            Assert.Equal("Nestle", brand.Name);
+        }
+    }
+}
